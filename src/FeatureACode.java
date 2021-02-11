846d6111a19f4a66149fb7283920be541abd58ff
HuntermanTeam <jonsnow9091.fs@gmail.com>
2021-02-11 14:16:09 +0200

    added 2

--- src/FeatureACode.java
+++ src/FeatureACode.java
@@ -5,6 +5,8 @@ public class FeatureACode {
     //i wanna say somethigelse
     public static void main(String[] args) {
         System.out.println("awesome work");
+
+        System.out.println("b21 is learning more things");
     }
 
 }
